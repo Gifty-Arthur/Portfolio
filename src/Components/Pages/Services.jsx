d3cc4a309f0745f4eabf26436c4f8331333faa00
Gifty Arthur <giftyarthur031@gmail.com>
2024-05-23 20:48:32 +0000

    udpate

--- src/Components/Pages/Services.jsx
+++ src/Components/Pages/Services.jsx
@@ -5,7 +5,7 @@ const Services = () => {
     <div className="bg-tertiary w-full flex flex-col items-center justify-center  h-[675px] bg-center">
       <div className="container">
         <div>
-          <h1 className="text-white text-[65px] font-poppins">Services</h1>
+          <h1 className=" text-white text-[65px] font-poppins">Services</h1>
         </div>
       </div>
     </div>
